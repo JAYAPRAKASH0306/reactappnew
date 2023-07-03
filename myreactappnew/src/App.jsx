48ceb3989cd4bb5e267a4eda0c847bed211dc14e
JAYAPRAKASH0306 <jayaprakashk0306@gmail.com>
2023-07-03 13:56:08 +0530

    myday22task

---
+++ myreactappnew/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+
+import Pricecard1 from './Componets/Pricecard/Pricecard';
+function App() {
+  
+  return (
+    <div className="App">
+         <Pricecard1 className="Pricecard1"/>
+      
+    </div>
+  );
+}
+
+export default App;
